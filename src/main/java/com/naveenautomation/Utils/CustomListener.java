27b389c0226c732c53ea7b889a08cce28d4a3ba2
SurabhiSadanand <surabhisadanand92@gmail.com>
2022-09-17 09:16:39 -0400

    Updates

--- src/main/java/com/naveenautomation/Utils/CustomListener.java
+++ src/main/java/com/naveenautomation/Utils/CustomListener.java
@@ -1,5 +1,6 @@
 package com.naveenautomation.Utils;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -30,6 +31,24 @@ public class CustomListener extends TestBase implements ITestListener {
 	public void onTestSkipped(ITestResult result) {
 
 		logger.info("Test execution has skipped...." + result.getMethod().getMethodName());
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
